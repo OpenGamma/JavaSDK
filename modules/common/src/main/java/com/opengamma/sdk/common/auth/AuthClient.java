69e1b8aec68f1cc65eadd19d9ee56df146d0b44c
Stephen Colebourne <stephen@opengamma.com>
2017-02-14 17:55:27 +0000

    Setup project to call remote service

---
+++ modules/common/src/main/java/com/opengamma/sdk/common/auth/AuthClient.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.sdk.common.auth;
+
+import com.opengamma.sdk.common.ServiceInvoker;
+
+/**
+ * Client providing access to the auth service.
+ * <p>
+ * Applications should not need to use this service directly.
+ * The {@link ServiceInvoker} invokes authentication when required.
+ */
+public interface AuthClient {
+
+  /**
+   * Obtains an instance.
+   * 
+   * @param invoker  the service invoker
+   * @return the client
+   */
+  public static AuthClient of(ServiceInvoker invoker) {
+    return InvokerAuthClient.of(invoker);
+  }
+
+  //-------------------------------------------------------------------------
+  /**
+   * Authenticates the user based on a username and password.
+   * 
+   * @param username  the username
+   * @param password  the password
+   * @return the result containing the access token
+   */
+  public abstract AccessTokenResult authenticatePassword(String username, String password);
+
+  /**
+   * Authenticates the user based on an API key.
+   * 
+   * @param apiKey  the API key
+   * @param apiKeySecret  the secret
+   * @return the result containing the access token
+   */
+  public abstract AccessTokenResult authenticateApiKey(String apiKey, String apiKeySecret);
+
+  /**
+   * Refreshes the main token based on the refresh token.
+   * 
+   * @param refreshToken  the refresh token
+   * @return the result containing the access token
+   */
+  public abstract AccessTokenResult refreshToken(String refreshToken);
+
+}
---
+++ modules/margin/src/test/java/com/opengamma/sdk/margin/TestingAuthClient.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2017 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.sdk.margin;
+
+import com.opengamma.sdk.common.auth.AccessTokenResult;
+import com.opengamma.sdk.common.auth.AuthClient;
+
+/**
+ * Mock auth client for testing.
+ */
+public final class TestingAuthClient implements AuthClient {
+
+  @Override
+  public AccessTokenResult authenticatePassword(String username, String password) {
+    if (username.equals("bad")) {
+      throw new IllegalStateException("User rejected: bad");
+    }
+    return AccessTokenResult.of("1234", "bearer", 60_000, "4321");
+  }
+
+  @Override
+  public AccessTokenResult authenticateApiKey(String apiKey, String apiKeySecret) {
+    if (apiKey.equals("bad")) {
+      throw new IllegalStateException("API key rejected: bad");
+    }
+    return AccessTokenResult.of("1234", "bearer", 60_000, "4321");
+  }
+
+  @Override
+  public AccessTokenResult refreshToken(String refreshToken) {
+    if (refreshToken.equals("bad")) {
+      throw new IllegalStateException("Refresh token rejected: bad");
+    }
+    return AccessTokenResult.of("1234", "bearer", 60_000, "4321");
+  }
+
+}
