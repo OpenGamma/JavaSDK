fe0806d2914b600e9b63dab7881ef03791d3e88d
Andrei Ruse <andrei@opengamma.com>
2017-12-18 14:08:50 +0000

    Adding missing copyright and JavaDocs

--- modules/margin/src/main/java/com/opengamma/sdk/margin/MarginOperation.java
+++ modules/margin/src/main/java/com/opengamma/sdk/margin/MarginOperation.java
@@ -1,13 +1,27 @@
+/*
+ * Copyright (C) 2017 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.sdk.margin;
 
 /**
  * Provides a list of Margin Service operations available in the SDK
  */
 public enum MarginOperation {
+  /** List CCPs operation */
   LIST_CCPS("List CCPs"),
+
+  /** Get CCPs information operation */
   GET_CCP_INFO("Get CCP Information"),
+
+  /** Create calculation operation */
   CREATE_CALCULATION("Create calculation"),
+
+  /** Get calculation operation */
   GET_CALCULATION("Get calculation"),
+
+  /** Delete calculation operation */
   DELETE_CALCULATION("Delete calculation");
 
   /**
