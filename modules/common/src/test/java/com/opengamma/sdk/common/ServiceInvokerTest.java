594510cc4dc58c836300e32cf4b13c027e676a5a
Andrei Ruse <andrei@opengamma.com>
2018-01-08 10:47:57 +0000

    Addressing PR feedback - license and newline. No functional changes

--- modules/common/src/test/java/com/opengamma/sdk/common/ServiceInvokerTest.java
+++ modules/common/src/test/java/com/opengamma/sdk/common/ServiceInvokerTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2018 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.sdk.common;
 
 import org.testng.annotations.AfterMethod;
